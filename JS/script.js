71107f27bfdd57d49d0fdbc9b64235190b3c05f9
Kushagra <kushagrathegame1@gmail.com>
2024-01-23 11:44:30 +0530

    removed type="module" from cript tags

--- JS/script.js
+++ JS/script.js
@@ -6,12 +6,12 @@
 //   backSpedd: 60,
 //   loop: true,
 // });
+// import Typed from 'typed.js'
 
-
-    var typed = new Typed(".typing", {
-        strings: [" ","Web Developer", "Web Designer", "Youtuber", "And More. . ."],
-        typeSpeed: 50,
-        loop: true,
-        backDelay: 900,
-        backSpeed: 30,
-    });
+new Typed(".typing", {
+  strings: ["Web Developer", "Web Designer", "Youtuber", "And More..."],
+  typeSpeed: 50,
+  loop: true,
+  backDelay: 900,
+  backSpeed: 30,
+});
